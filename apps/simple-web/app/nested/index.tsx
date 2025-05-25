91e10c51418a79c9e15a30fe36fee565105dde5a
M Gilang Januar <mgilangjanuar@gmail.com>
2025-05-25 16:58:42 +0700

    feat: add InnerPage component and update NestedIndex links for navigation

--- apps/simple-web/app/nested/index.tsx
+++ apps/simple-web/app/nested/index.tsx
@@ -5,6 +5,7 @@ export default function NestedIndex() {
     <h3>Nested Index Page</h3>
     <p>
       <Link href="-1">Back</Link>
+      <Link href="/nested/inner">Inner Page</Link>
     </p>
   </div>
 }
---
+++ apps/simple-web/app/nested/inner/index.tsx
@@ -0,0 +1,7 @@
+export default function InnerPage() {
+  return (
+    <div>
+      <p>This is the inner page of the nested layout.</p>
+    </div>
+  )
+}

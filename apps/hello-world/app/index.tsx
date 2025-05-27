43f938bc44ee38972277305459624364b61a57a2
M Gilang Januar <mgilangjanuar@gmail.com>
2025-05-27 17:51:25 +0700

    feat: update HomePage layout and modify package.json dependencies for consistency

--- apps/hello-world/app/index.tsx
+++ apps/hello-world/app/index.tsx
@@ -2,11 +2,11 @@ import Logo from '@/logo.svg' with { type: 'text' }
 import { Svg } from 'buntal'
 
 export default function HomePage() {
-  return <div className="min-h-svh flex flex-col items-center justify-center">
-    <div className="container mx-auto w-fit space-y-4">
+  return <div className="min-h-svh flex flex-col items-center justify-center container mx-auto">
+    <div className="space-y-4">
       <Svg src={Logo} className="[&>svg]:size-28" />
-      <h1 className="text-4xl font-bold">Hello, Buntal!</h1>
-      <p className="text-lg mb-8">This is a simple Bun app using Buntal.</p>
+      <h1 className="text-4xl font-bold">Next.js who?</h1>
+      <p className="text-lg">This is a simple Bun app using Buntal.</p>
     </div>
   </div>
 }

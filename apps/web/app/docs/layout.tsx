3748559705f74b9644fab96217750cff34807974
M Gilang Januar <mgilangjanuar@gmail.com>
2025-05-28 08:53:41 +0700

    fix: refactor DocsLayout to use MENUS array for menu items and improve spacing

--- apps/web/app/docs/layout.tsx
+++ apps/web/app/docs/layout.tsx
@@ -1,9 +1,19 @@
 import Logo from '@/app/logo.svg' with { type: 'text' }
-import { Link, Svg } from 'buntal'
+import { cn } from '@/lib/utils'
+import { Link, Svg, useRouter } from 'buntal'
+
+const MENUS = [
+  {
+    title: 'Get Started',
+    href: '/docs',
+  }
+]
 
 export default function DocsLayout({ children }: Readonly<{
   children: React.ReactNode,
 }>) {
+  const { pathname } = useRouter()
+
   return (
     <div className="drawer lg:drawer-open">
       <input id="docs-layout-drawer" type="checkbox" className="drawer-toggle" />
@@ -13,20 +23,22 @@ export default function DocsLayout({ children }: Readonly<{
       <div className="drawer-side">
         <label htmlFor="docs-layout-drawer" aria-label="close sidebar" className="drawer-overlay"></label>
         <ul className="menu bg-base-200 text-base-content min-h-full w-80 p-4 gap-0.5">
-          <Link href="/" className="flex items-center gap-4 px-3 !h-10 group mb-2">
+          <Link href="/" className="flex items-center gap-4 px-3 !h-10 group mb-3.5">
             <Svg src={Logo} className="[&>*]:size-10" />
             <div className="flex flex-col -space-y-1">
               <h3 className="font-semibold text-lg group-hover:underline underline-offset-2">Buntal JS</h3>
               <span className="text-xs text-base-content/60">v0.0.3</span>
             </div>
           </Link>
-          <li>
-            <span className="grid grid-cols-1">
-              <Link href="/docs" className="truncate">
-                Get Started
-              </Link>
-            </span>
-          </li>
+          {MENUS.map((menu, i) => (
+            <li key={i}>
+              <span className={cn('grid grid-cols-1', pathname === menu.href && 'menu-active')}>
+                <Link href={menu.href} className="truncate">
+                  {menu.title}
+                </Link>
+              </span>
+            </li>
+          ))}
         </ul>
       </div>
     </div>

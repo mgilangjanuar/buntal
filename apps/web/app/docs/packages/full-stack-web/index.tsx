a386f601c92abe8747ed37a8e602332bc5323bae
M Gilang Januar <mgilangjanuar@gmail.com>
2025-05-29 17:56:36 +0700

    fix: enhance Quick Start documentation with project initialization command and structure details

--- apps/web/app/docs/packages/full-stack-web/index.tsx
+++ apps/web/app/docs/packages/full-stack-web/index.tsx
@@ -23,6 +23,17 @@ export default function HTTPPkgPage() {
         <div className="container ml-0 prose pb-6 grid grid-cols-1">
           <section>
             <h2 className="mt-0">Quick Start</h2>
+            <p>
+              After initializing a project from a template using this command:
+            </p>
+            <SyntaxHighlighter
+              language="sh"
+              style={theme === 'dark' ? atomOneDark : atomOneLight}
+              customStyle={{ padding: '12px 16px' }}
+            >
+              {`bun create buntal@latest my-app`}
+            </SyntaxHighlighter>
+            <p>You will have this project structure:</p>
             <SyntaxHighlighter
               language="sh"
               style={theme === 'dark' ? atomOneDark : atomOneLight}
@@ -43,6 +54,44 @@ export default function HTTPPkgPage() {
 ├── public
 └── tsconfig.json`}
             </SyntaxHighlighter>
+            <p>
+              Seems familiar, right? The main modules are located in the{' '}
+              <code>app</code> directory, which contains the main entry
+              point/page at <code>index.tsx</code>, <code>globals.css</code>,
+              and a layout file at <code>layout.tsx</code>.
+            </p>
+            <p>
+              You can create a new page such as <code>about/index.tsx</code> in
+              the <code>app</code> directory, and it will be automatically
+              available at <code>/about</code>.
+            </p>
+            <p>
+              Run the development server using <code>bun dev</code>, and the
+              output will look like this:
+            </p>
+            <SyntaxHighlighter
+              language="sh"
+              style={theme === 'dark' ? atomOneDark : atomOneLight}
+              customStyle={{ padding: '12px 16px' }}
+            >
+              {`  -... ..- -. - .- .-..
+  Local:        http://localhost:3000
+  Network:      http://192.168.18.121:3000
+
+Done in 12ms`}
+            </SyntaxHighlighter>
+            <p>
+              Explore the full example on{' '}
+              <a
+                href="https://github.com/mgilangjanuar/buntal/tree/main/examples/hello-world"
+                className="underline-offset-4"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                GitHub
+              </a>
+              .
+            </p>
           </section>
           <p className="text-sm text-base-content/60 border-t border-base-content/10 pt-6 mt-12">
             Last modified: 2025-05-29

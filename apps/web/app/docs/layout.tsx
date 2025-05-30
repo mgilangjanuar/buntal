2e3cdef33276a3cc1ba8ba61aa4dd5a507227f35
M Gilang Januar <mgilangjanuar@gmail.com>
2025-05-30 08:30:16 +0700

    fix: update version to v0.0.6 in package.json and layout files

--- apps/web/app/docs/layout.tsx
+++ apps/web/app/docs/layout.tsx
@@ -57,7 +57,7 @@ export default function DocsLayout({
               <h3 className="font-semibold text-lg group-hover:underline underline-offset-2">
                 Buntal JS
               </h3>
-              <span className="text-xs text-base-content/60">v0.0.5</span>
+              <span className="text-xs text-base-content/60">v0.0.6</span>
             </div>
           </Link>
           {MENUS.map((menu, i) => (

797a27ed5147ac9630bac2e50acc25e9ec9740e1
M Gilang Januar <mgilangjanuar@gmail.com>
2025-06-05 07:10:24 +0700

    feat: add copy-to-clipboard functionality to code component

--- apps/web/components/code.tsx
+++ apps/web/components/code.tsx
@@ -1,4 +1,5 @@
 import { useTheme } from '@/hooks/use-theme'
+import { useState } from 'react'
 import SyntaxHighlighter from 'react-syntax-highlighter'
 import {
   atomOneDark,
@@ -13,14 +14,60 @@ export default function Code({
   children: string
 }) {
   const { theme } = useTheme()
+  const [copiedText, setCopiedText] = useState<string | null>(null)
 
   return (
-    <SyntaxHighlighter
-      language={language}
-      style={theme === 'dark' ? atomOneDark : atomOneLight}
-      customStyle={{ padding: '12px 16px' }}
-    >
-      {children.trim()}
-    </SyntaxHighlighter>
+    <div className="relative">
+      <button
+        className="btn btn-sm btn-square btn-ghost absolute top-2 right-2 opacity-70 hover:opacity-100"
+        onClick={() => {
+          navigator.clipboard.writeText(children.trim())
+          setCopiedText(children.trim())
+          setTimeout(() => setCopiedText(null), 2000)
+        }}
+      >
+        {copiedText === children.trim() ? (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            className="!size-4"
+          >
+            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+            <path d="M5 12l5 5l10 -10" />
+          </svg>
+        ) : (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            className="!size-4"
+          >
+            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+            <path d="M7 7m0 2.667a2.667 2.667 0 0 1 2.667 -2.667h8.666a2.667 2.667 0 0 1 2.667 2.667v8.666a2.667 2.667 0 0 1 -2.667 2.667h-8.666a2.667 2.667 0 0 1 -2.667 -2.667z" />
+            <path d="M4.012 16.737a2.005 2.005 0 0 1 -1.012 -1.737v-10c0 -1.1 .9 -2 2 -2h10c.75 0 1.158 .385 1.5 1" />
+          </svg>
+        )}
+      </button>
+      <SyntaxHighlighter
+        language={language}
+        style={theme === 'dark' ? atomOneDark : atomOneLight}
+        customStyle={{ padding: '12px 16px' }}
+      >
+        {children.trim()}
+      </SyntaxHighlighter>
+    </div>
   )
 }

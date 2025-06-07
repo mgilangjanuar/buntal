b07b5af754f91f94ad4bd0210dcc63d8d8120351
M Gilang Januar <mgilangjanuar@gmail.com>
2025-06-07 17:29:27 +0700

    fix: simplify Feature component by converting to arrow function and removing unnecessary return statement

--- apps/web/app/index.tsx
+++ apps/web/app/index.tsx
@@ -202,31 +202,27 @@ const Feature = ({
   description: string
   icon: React.ReactNode
   index: number
-}) => {
-  return (
-    <div
-      className={cn(
-        'flex flex-col lg:border-r min-h-[197px] lg:min-h-[244px] xl:min-h-[197px] py-10 relative group/feature',
-        (index === 0 || index === 4) && 'lg:border-l',
-        index < 4 && 'lg:border-b',
-        'dark:border-neutral-800 border-neutral-200'
-      )}
-    >
-      <div className="mb-4 relative z-10 px-10 text-base-content/80">
-        {icon}
-      </div>
-      <div className="text-lg font-bold mb-2 relative z-10 px-10">
-        <div className="absolute left-0 inset-y-0 h-6 group-hover/feature:h-8 w-1 rounded-tr-full rounded-br-full bg-neutral-300 dark:bg-neutral-700 group-hover/feature:bg-blue-500 transition-all duration-200 origin-center" />
-        <span className="group-hover/feature:translate-x-2 transition duration-200 inline-block text-base-content">
-          {title}
-        </span>
-      </div>
-      <p className="text-sm text-base-content/80 max-w-xs relative z-10 px-10">
-        {description}
-      </p>
+}) => (
+  <div
+    className={cn(
+      'flex flex-col lg:border-r min-h-[197px] lg:min-h-[244px] xl:min-h-[197px] py-10 relative group/feature',
+      (index === 0 || index === 4) && 'lg:border-l',
+      index < 4 && 'lg:border-b',
+      'dark:border-neutral-800 border-neutral-200'
+    )}
+  >
+    <div className="mb-4 relative z-10 px-10 text-base-content/80">{icon}</div>
+    <div className="text-lg font-bold mb-2 relative z-10 px-10">
+      <div className="absolute left-0 inset-y-0 h-6 group-hover/feature:h-8 w-1 rounded-tr-full rounded-br-full bg-neutral-300 dark:bg-neutral-700 group-hover/feature:bg-blue-500 transition-all duration-200 origin-center" />
+      <span className="group-hover/feature:translate-x-2 transition duration-200 inline-block text-base-content">
+        {title}
+      </span>
     </div>
-  )
-}
+    <p className="text-sm text-base-content/80 max-w-xs relative z-10 px-10">
+      {description}
+    </p>
+  </div>
+)
 
 export default function HomePage() {
   const { theme } = useTheme()

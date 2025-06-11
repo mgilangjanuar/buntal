7548bc719694174f4cd172ed4cf632e6f8b67e08
Sony AK <sony@sony-ak.com>
2025-06-11 20:35:30 +0700

    feat: enhance project name input handling for better user experience, now support bun create buntal@latest without project name and ask it by prompt

--- packages/create-buntal/index.ts
+++ packages/create-buntal/index.ts
@@ -3,11 +3,32 @@
 import { program } from 'commander'
 import { createProject } from './cmd/default'
 
+async function promptForProjectName() {
+  process.stdout.write('Please enter the project name: ')
+  const input = await new Promise<string>((resolve) => {
+    process.stdin.once('data', (data) => resolve(data.toString().trim()))
+  })
+  return input
+}
+
 program
   .name('create-buntal')
   .description('Create a new Buntal project')
   .version('0.0.1')
 
-program.argument('<project-name>', 'Name of the project').action(createProject)
+program
+  .argument('[project-name]', 'Name of the project')
+  .action(async (projectName) => {
+    if (!projectName) {
+      projectName = await promptForProjectName()
+
+      if (!projectName) {
+        console.error('Project name is required.')
+        process.exit(1)
+      }
+    }
+    await createProject(projectName)
+    process.exit(0)
+  })
 
 program.parse()

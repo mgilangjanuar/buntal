224e3f452ccf21f90ee16dc02b03c4f01e425516
Sony AK <sony@sony-ak.com>
2025-06-11 22:18:53 +0700

    feat: enhance create-buntal, user able to omit <project-name> (#29)
    * feat: enhance project name input handling for better user experience, now support bun create buntal@latest without project name and ask it by prompt
    
    * fix: improve error handling for missing project name prompt

--- packages/create-buntal/index.ts
+++ packages/create-buntal/index.ts
@@ -3,11 +3,30 @@
 import { program } from 'commander'
 import { createProject } from './cmd/default'
 
+async function promptForProjectName() {
+  process.stdout.write('Please enter the project name: ')
+  const input = await new Promise<string>((resolve) => {
+    process.stdin.once('data', (data) => resolve(data.toString().trim()))
+  })
+  return input
+}
+
 program
   .name('create-buntal')
   .description('Create a new Buntal project')
   .version('0.0.1')
 
-program.argument('<project-name>', 'Name of the project').action(createProject)
+program
+  .argument('[project-name]', 'Name of the project')
+  .action(async (projectName) => {
+    if (!projectName) {
+      projectName = await promptForProjectName()
+
+      if (!projectName) {
+        program.error('Project name is required.')
+      }
+    }
+    await createProject(projectName)
+  })
 
 program.parse()

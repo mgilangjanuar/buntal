1a5e30c080cefce4fd4321c44a20e8c94c67afa2
M Gilang Januar <mgilangjanuar@gmail.com>
2025-06-18 21:58:03 +0700

    feat(blog): implement MDX support for dynamic posts
    - Added a new server-side function to handle MDX file compilation and rendering.
    - Updated the Post component to display content from MDX files.
    - Configured Buntal to use MDX with esbuild.
    - Added necessary dependencies for MDX processing.
    - Created an example MDX post for demonstration.

--- examples/blog/app/blog/[[...slug]]/index.tsx
+++ examples/blog/app/blog/[[...slug]]/index.tsx
@@ -1,12 +1,43 @@
-export default function Post() {
+import { Req } from '@buntal/http'
+import { compile } from '@mdx-js/mdx'
+import path from 'path'
+import { renderToStaticMarkup } from 'react-dom/server'
+
+export const $ = async (req: Req) => {
+  const slug = req.params.slug
+  if (!slug) return
+
+  const mdPath = path.join(__dirname, slug + '.mdx')
+
+  if (!(await Bun.file(mdPath).exists())) return
+
+  const mdContent = await Bun.file(mdPath).text()
+  const blob = await compile(mdContent).then((code) => {
+    console.log('Blob created:', code)
+    const res = new Blob([code.value], { type: 'application/javascript' })
+    return res
+  })
+
+  const url = URL.createObjectURL(blob)
+
+  const component = await import(url)
+  return {
+    component: renderToStaticMarkup(<component.default />)
+  }
+}
+
+export default function Post({
+  data
+}: Readonly<{
+  data?: Awaited<ReturnType<typeof $>>
+}>) {
   return (
     <div className="container mx-auto prose">
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestiae
-        adipisci, laboriosam nisi eaque, fugiat accusamus dolore sed fuga
-        tempore, voluptates quos molestias quam consequatur repellendus
-        consequuntur assumenda maiores rem eum.
-      </p>
+      {/* eslint-disable-next-line @eslint-react/dom/no-dangerously-set-innerhtml */}
+      {/* <data dangerouslySetInnerHTML={{
+        __html: data?.component || ''
+      }}></data> */}
+      {data?.component || ''}
     </div>
   )
 }
--- examples/blog/buntal.config.ts
+++ examples/blog/buntal.config.ts
@@ -1,5 +1,12 @@
+import mdx from '@mdx-js/esbuild'
+import type { BunPlugin } from 'bun'
 import type { BuntalConfig } from 'buntal'
 
-const config = {} satisfies BuntalConfig
+const config = {
+  config: {
+    target: 'bun',
+    plugins: [mdx() as unknown as BunPlugin]
+  }
+} satisfies BuntalConfig
 
 export default config

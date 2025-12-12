5786d732ebc6357145f7b8423c8d9f540b20ca80
M Gilang Januar <mgilangjanuar@gmail.com>
2025-12-12 13:17:01 +0700

    fix: security patch (CVSS 7.5 and CVSS 5.3)

--- apps/web/app/references/layout.tsx
+++ apps/web/app/references/layout.tsx
@@ -177,7 +177,7 @@ export default function ReferenceLayout({
               <h3 className="font-semibold text-lg group-hover:underline underline-offset-2 font-serif">
                 Buntal JS
               </h3>
-              <span className="text-xs text-base-content/60">v0.1.22</span>
+              <span className="text-xs text-base-content/60">v0.1.23</span>
             </div>
           </Link>
           {MENUS.map((menu) => (
